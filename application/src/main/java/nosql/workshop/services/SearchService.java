6baece3ec6e9f4b0f33b703906cf11e8cc6de7c0
Louspirit <guillaume.escarieux@free.fr>
2015-02-18 10:37:45 +0100

    Application:  Implémentation services ES : - getTownLocation : OK

--- application/src/main/java/nosql/workshop/services/SearchService.java
+++ application/src/main/java/nosql/workshop/services/SearchService.java
@@ -6,15 +6,20 @@ import com.google.inject.Inject;
 import com.google.inject.name.Named;
 import nosql.workshop.model.Installation;
 import nosql.workshop.model.suggest.TownSuggest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.ImmutableSettings;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -27,6 +32,9 @@ public class SearchService {
     public static final String ES_HOST = "es.host";
     public static final String ES_TRANSPORT_PORT = "es.transport.port";
     private static final String TOWN_TYPE = "town";
+    public static final String KEY_LOCATION = "location";
+    public static final String FIELD_TOWN_NAME = "townName";
+
     final Client elasticSearchClient;
     final ObjectMapper objectMapper;
 
@@ -70,7 +78,24 @@ public class SearchService {
     }
 
     public Double[] getTownLocation(String townName) {
-        // TODO codez le service
-        throw new UnsupportedOperationException();
+        Double[] coordinates = new Double[2];
+
+        SearchResponse response = elasticSearchClient.prepareSearch(TOWNS_INDEX)
+                .setTypes(TOWN_TYPE)
+                .setSearchType(SearchType.QUERY_AND_FETCH)
+                .setQuery(QueryBuilders.queryString(FIELD_TOWN_NAME + ":" + townName)) // Query
+                .execute()
+                .actionGet();
+        SearchHit[] searchHits = response.getHits().getHits();
+
+        if(searchHits.length>0){
+            Map<String,Object> result = searchHits[0].getSource();
+            ArrayList<Double> location = (ArrayList<Double>) result.get(KEY_LOCATION);
+            coordinates[0]= location.get(0);
+            coordinates[1]= location.get(1);
+        }
+        return coordinates;
     }
+
+
 }
